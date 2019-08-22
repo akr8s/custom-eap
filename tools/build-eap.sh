657f5e6ae1d14ce109f7c7bd7a12e690ae41bf0e
zhengbin <zhengbin78@hotmail.com>
2019-08-22 21:01:32 +0800

    u

--- tools/build-eap.sh
+++ tools/build-eap.sh
@@ -45,9 +45,9 @@ bin/jboss-cli.sh "patch apply /opt/jboss/jboss-eap-$EAP_PATCH_VERSION-patch.zip"
 rm -f /opt/jboss/jboss-*.zip
 
 # ADD password vault
-mkdir /opt/jboss/eap/vault
-keytool -genseckey -alias vault -storetype jceks -keyalg AES -keysize 128 -storepass vault-xiugaiwo -keypass vault-xiugaiwo -validity 3650 -keystore /opt/jboss/eap/vault/vault.keystore
-/opt/jboss/eap/bin/vault.sh -k /opt/jboss/eap/vault/vault.keystore  -p vault-xiugaiwo --alias vault -b vb -a password -x $DB_PASSWORD --enc-dir /opt/jboss/eap/vault/ --iteration 120 --salt qianhai1
+#mkdir /opt/jboss/eap/vault
+#keytool -genseckey -alias vault -storetype jceks -keyalg AES -keysize 128 -storepass vault-xiugaiwo -keypass vault-xiugaiwo -validity 3650 -keystore /opt/jboss/eap/vault/vault.keystore
+#/opt/jboss/eap/bin/vault.sh -k /opt/jboss/eap/vault/vault.keystore  -p vault-xiugaiwo --alias vault -b vb -a password -x $DB_PASSWORD --enc-dir /opt/jboss/eap/vault/ --iteration 120 --salt qianhai1
 
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/standalone-configuration.cli
 rm -rf /opt/jboss/eap/standalone/configuration/standalone_xml_history
